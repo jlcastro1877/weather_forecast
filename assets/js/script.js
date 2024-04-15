47964a741b7deee2af1130ababd5e15d72228ac7
jlcastro1877 <jlcastro1877@gmail.com>
2024-04-14 23:16:56 -0600

    JavaScript code functions draft done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "77a60fde9123837e97948e31963ac6dd";
 
+weatherForm.addEventListener("submit", (event) => {});
+
+async function getWeatherData(city) {}
+
+function displayWeatherInfo(data) {}
+
+function getWeatherEmoji(weatherId) {}
+
+function displayError(message) {}
